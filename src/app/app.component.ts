39379149f7399442a75e616c2e85ee69dec256bb
Konstantine <icon.dnepr@gmail.com>
2020-06-27 20:07:25 +0300

    add operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import {interval, Subscription} from "rxjs";
+import {map} from 'rxjs/operators'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'angular-basics';
+  title = 'angular RxJS';
+
+  sub: Subscription
+
+  constructor() {
+      const myTimer$ = interval(1000)
+      this.sub = myTimer$.pipe(
+        map((value, index )=>{
+            return 'номер: ' + value
+        })
+      ).subscribe(value => console.log(value))
+  }
+
+  stop(){
+      this.sub.unsubscribe()
+  }
 }
+
