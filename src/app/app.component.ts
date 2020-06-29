ee5de250fd8c63789d408a964403aeb0b3d801ce
Konstantine <icon.dnepr@gmail.com>
2020-06-29 10:53:39 +0300

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CounterService} from "./services/counter.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
+  title = 'Angular Services';
+
+  constructor( public counterService : CounterService ) {}
+
 }
---
+++ src/app/services/counter.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CounterService {
+
+    counter = 0
+
+    increment(){
+        this.counter++
+    }
+
+    decrement(){
+        this.counter--
+    }
+
+}
