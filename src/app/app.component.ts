30743ab932c130ad316e045427a33bd2209b3f8d
Konstantine <icon.dnepr@gmail.com>
2020-06-29 15:37:12 +0300

    add forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-basics';
+export class AppComponent implements OnInit {
+    title = 'Angular Forms';
+
+    myform: FormGroup;
+    ngOnInit() {
+        this.myform = new FormGroup({
+            email: new FormControl('', [Validators.required, Validators.email]),
+            password: new FormControl('', [
+                Validators.required,
+                Validators.minLength(6),
+            ]),
+            adress: new FormGroup({
+                country: new FormControl('ua'),
+                city: new FormControl('', Validators.required),
+            }),
+            skills: new FormArray([]),
+        });
+    }
+
+    submit() {
+        if (!this.myform.invalid) {
+            console.log(this.myform);
+            const formData = { ...this.myform.value };
+            console.log('Данные формы: ', formData);
+        }
+    }
+
+    setCapital() {
+        const capital = {
+            ru: 'маасква',
+            ua: 'Киев',
+            by: 'Минск',
+        };
+        const cityKey = this.myform.get('adress').get('country').value;
+        const cityCap = capital[cityKey];
+
+        this.myform.patchValue({
+            adress: { city: cityCap },
+        });
+    }
+
+    addSkills() {
+        console.log(this.myform.get('skills'));
+        const control = new FormControl('', Validators.required);
+        (this.myform.get('skills') as FormArray).push(control);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+
+    imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+    providers: [],
+
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
